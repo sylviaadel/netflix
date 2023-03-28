b3e9ba055a4fa8cabd3cbe090b155a9a32101c2b
Sylvia Tadross <sylvia.adel.emil@gmail.com>
2023-03-28 21:42:53 +0200

    Add font awesome and navbar

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,9 +1,40 @@
+import { useState } from "react";
+import { useUser } from "../../state/UsersProvider";
+import { useNavigate } from "react-router";
+import InfoPopup from "../modal/InfoPopup";
+import Modal from "../modal/Modal";
 import logo from "../../assets/images/logo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { icon } from "@fortawesome/fontawesome-svg-core/import.macro";
+import { logoutInfo } from "../../scripts/helpers";
 
 export default function Navbar() {
+  const navigate = useNavigate();
+  const { uid, setUid, saveUID } = useUser();
+  const [modal, setModal] = useState(null);
+  const popup = (
+    <InfoPopup setModal={setModal} onClose={logoutUser} item={logoutInfo} />
+  );
+
+  function logoutUser() {
+    setUid("");
+    saveUID("");
+    navigate("/");
+  }
+  function onChange() {
+    if (uid) {
+      setModal(popup);
+    } else {
+      navigate("/login");
+    }
+  }
   return (
     <section id="Navbar">
       <img src={logo} alt="Red Netflix logo word" />
+      <div className="login-btn" onClick={() => onChange()}>
+        <FontAwesomeIcon icon={icon({ name: "user-circle" })} />
+      </div>
+      <Modal state={[modal, setModal]} />
     </section>
   );
 }
--- src/scripts/helpers.js
+++ src/scripts/helpers.js
@@ -1,2 +1,8 @@
 export const recoverMessage =
   "Email with a reset link sent. Please check your SPAM/Junk folder as well.";
+
+export const logoutInfo = {
+  title: "Confirmation",
+  message: "Are you sure you want to logout?",
+  btnTitle: "Logout",
+};
