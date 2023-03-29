4eee68a87e7e6e4b1da861c3106b28aa20c837f0
Sylvia Tadross <sylvia.adel.emil@gmail.com>
2023-03-29 19:01:56 +0200

    add details of hero section

--- src/components/landing/Hero.jsx
+++ src/components/landing/Hero.jsx
@@ -1,10 +1,20 @@
 export default function Hero({ item }) {
-  const { heading, logo, background, description } = item;
+  const { heading, logo, background, description, videoLink } = item;
 
   return (
     <section id="Hero">
       <div className="background"></div>
       <img src={background} alt={heading} />
+      <div className="details">
+        <img src={logo} />
+        <p>{description}</p>
+        <button className="white-btn">
+          <i class="fa-solid fa-play"></i> Play
+        </button>
+        <a href={videoLink} className="grey-btn">
+          <i class="fa-solid fa-info"></i> More Info
+        </a>
+      </div>
     </section>
   );
 }
