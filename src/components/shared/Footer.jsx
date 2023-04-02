3236ee908c8742305672181256ba33b3f338cd1c
Sylvia Tadross <sylvia.adel.emil@gmail.com>
2023-04-03 00:27:44 +0200

    Add footer

---
+++ src/components/shared/Footer.jsx
@@ -0,0 +1,31 @@
+export default function Footer() {
+  return (
+    <section id="Footer">
+      <p>
+        Questions? Call <a href="tel:0201 604 328"></a>0201 604 328
+      </p>
+      <ul>
+        <li>
+          <a href="https://help.netflix.com/en/node/412">FAQ</a>
+        </li>
+        <li>
+          <a href="https://help.netflix.com/en/">Help Centre</a>
+        </li>
+        <li>
+          <a href="https://help.netflix.com/legal/termsofuse">Terms of Use </a>
+        </li>
+        <li>
+          <a href="https://help.netflix.com/legal/privacy">Privacy</a>
+        </li>
+        <li>
+          <a href="https://www.netflix.com/se-en/login">Cookie Preferences</a>
+        </li>
+        <li>
+          <a href="https://help.netflix.com/legal/corpinfo">
+            Corporate Information
+          </a>
+        </li>
+      </ul>
+    </section>
+  );
+}
--- src/routes/UserRoutes.jsx
+++ src/routes/UserRoutes.jsx
@@ -1,13 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 import Landing from "../pages/Landing";
 import NotFound from "../pages/NotFound";
+import Footer from "../components/shared/Footer";
 
 export default function UserRoutes() {
   return (
-    <Routes>
-      <Route path="/" element={<Landing />} />
-      <Route path="/landing" element={<Landing />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/landing" element={<Landing />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </>
   );
 }
