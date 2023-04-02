dd405fb41fc12f27da90d68f7b2cd525ce0f8024
Sylvia Tadross <sylvia.adel.emil@gmail.com>
2023-04-02 23:59:35 +0200

    Add episodes and fix styles

--- src/components/modal/Seasons.jsx
+++ src/components/modal/Seasons.jsx
@@ -1,9 +1,12 @@
 import { useState, useEffect } from "react";
 import { readSubCollection } from "../../scripts/fireStore/readSubCollection";
+import Episode from "./Episode";
 
 export default function Seasons({ id, collection }) {
   const [seasons, setSeasons] = useState([]);
   const [status, setStatus] = useState(0);
+  const [currentSeason, setCurrentSeason] = useState("CB2T7OR2ibPCUKHVAiUa");
+  const [episodes, setEpisodes] = useState([]);
 
   useEffect(() => {
     loadData(collection);
@@ -11,6 +14,10 @@ export default function Seasons({ id, collection }) {
 
   async function loadData(collection) {
     const data = await readSubCollection(`${collection}/${id}/seasons`);
+    const currentEpisodes = await readSubCollection(
+      `${collection}/${id}/seasons/${currentSeason}/episodes`
+    );
+    setEpisodes(currentEpisodes);
     onSuccess(data);
   }
 
@@ -19,18 +26,28 @@ export default function Seasons({ id, collection }) {
     setStatus(1);
   }
 
+  function handleChangeSeason(e) {
+    setCurrentSeason(e.target.value);
+    console.log(currentSeason);
+  }
+
   const Seasons = seasons.map((data) => (
     <option key={data.id} value={data.id}>
       {data.title}
     </option>
   ));
 
+  const Episodes = episodes.map((item) => (
+    <Episode key={item.id} item={item} />
+  ));
+
   return (
     <section className="seasons">
       <h3>Episodes</h3>
       <span className="select-wrapper">
-        <select>{Seasons}</select>
+        <select onChange={(e) => handleChangeSeason(e)}>{Seasons}</select>
       </span>
+      <div className="episodes">{Episodes}</div>
     </section>
   );
 }
