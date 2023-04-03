b30eebbea53ad4ad3275f3d93c5f12b71cc7dd48
Sylvia Tadross <sylvia.adel.emil@gmail.com>
2023-04-03 17:19:51 +0200

    Add footer json data

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -1,31 +1,18 @@
+import data from "../../data/footerData.json";
+
 export default function Footer() {
+  const FooterData = data.map((item) => (
+    <li>
+      <a href={item.link}>{item.title}</a>
+    </li>
+  ));
+
   return (
     <section id="Footer">
       <p>
         Questions? Call <a href="tel:0201 604 328"></a>0201 604 328
       </p>
-      <ul>
-        <li>
-          <a href="https://help.netflix.com/en/node/412">FAQ</a>
-        </li>
-        <li>
-          <a href="https://help.netflix.com/en/">Help Centre</a>
-        </li>
-        <li>
-          <a href="https://help.netflix.com/legal/termsofuse">Terms of Use </a>
-        </li>
-        <li>
-          <a href="https://help.netflix.com/legal/privacy">Privacy</a>
-        </li>
-        <li>
-          <a href="https://www.netflix.com/se-en/login">Cookie Preferences</a>
-        </li>
-        <li>
-          <a href="https://help.netflix.com/legal/corpinfo">
-            Corporate Information
-          </a>
-        </li>
-      </ul>
+      <ul>{FooterData}</ul>
     </section>
   );
 }
