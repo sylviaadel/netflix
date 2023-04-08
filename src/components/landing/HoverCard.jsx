1f2239bcd9248adef0b9e6954e69bb67885d657b
Sylvia Tadross <sylvia.adel.emil@gmail.com>
2023-04-09 00:40:50 +0200

    Add more details to card

--- src/components/landing/HoverCard.jsx
+++ src/components/landing/HoverCard.jsx
@@ -2,7 +2,7 @@ import imgIcon from "../../assets/images/camera-icon.png";
 import YoutubeEmbed from "../../scripts/YoutubeEmbed";
 
 export default function HoverCard({ item, details, setModal }) {
-  const { heading, background, logo, videoLink } = item;
+  const { heading, background, logo, videoLink, matches, year, genres } = item;
 
   function openVideo() {
     setModal(<YoutubeEmbed embedId={videoLink} />);
@@ -19,6 +19,11 @@ export default function HoverCard({ item, details, setModal }) {
         <button onClick={details} className="info-btn">
           <i className="fa-solid fa-chevron-down"></i>
         </button>
+        <section className="card-more-details">
+          <span>{matches}% Match</span>
+          <span>{year}</span>
+          <p>{genres}</p>
+        </section>
       </div>
     </div>
   );
