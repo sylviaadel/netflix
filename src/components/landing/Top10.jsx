267883ac393e40ae5d593a74780554b50e9199b2
Sylvia Tadross <sylvia.adel.emil@gmail.com>
2023-04-09 23:41:47 +0200

    Add slick

--- src/components/landing/Top10.jsx
+++ src/components/landing/Top10.jsx
@@ -2,6 +2,10 @@ import { useState, useEffect } from "react";
 import { readDocuments } from "../../scripts/fireStore/readDocuments";
 import { useItems } from "../../state/ItemsProvider";
 import TopTenItem from "./TopTenItem";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { SampleNextArrow, SamplePrevArrow } from "../shared/SliderArrows";
 
 export default function Top10() {
   const { data, dispatch } = useItems();
@@ -9,6 +13,40 @@ export default function Top10() {
   const [modal, setModal] = useState(null);
   const collection = "titles";
   const topTen = data.filter((item) => item.topTen === true);
+
+  var settings = {
+    centerPadding: "60px",
+    infinite: false,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    initialSlide: -0.25,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+    ],
+  };
 
   useEffect(() => {
     loadData(collection);
@@ -32,7 +70,9 @@ export default function Top10() {
   return (
     <section className="top10-container">
       <h3>Top 10 TV in Sweden Today</h3>
-      <ol>{Items}</ol>
+      <ol>
+        <Slider {...settings}>{Items}</Slider>
+      </ol>
     </section>
   );
 }
---
+++ src/components/shared/SliderArrows.jsx
@@ -0,0 +1,9 @@
+export function SampleNextArrow(props) {
+  const { className, onClick } = props;
+  return <div className={className} onClick={onClick}></div>;
+}
+
+export function SamplePrevArrow(props) {
+  const { className, onClick } = props;
+  return <div className={className} onClick={onClick}></div>;
+}
