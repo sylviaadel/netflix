8ef77f631d5c2526b1258aa655a2c57def8a6e53
Sylvia Tadross <sylvia.adel.emil@gmail.com>
2023-04-10 14:18:02 +0200

    Add condition for search results

--- src/components/landing/TitlesContainer.jsx
+++ src/components/landing/TitlesContainer.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import TitleItem from "./TitleItem";
 import Title from "./Title";
 import Modal from "../modal/Modal";
+import NoResults from "../shared/NoResults";
 
 export default function TitlesContainer({ movies, series, doc, query }) {
   const [modal, setModal] = useState(null);
@@ -46,12 +47,34 @@ export default function TitlesContainer({ movies, series, doc, query }) {
         type="documentary"
       />
     ));
+  console.log(Movies.length);
 
   return (
     <section className="titles-container">
-      <Title title="Movies" list={Movies} setModal={setModal} type="movie" />
-      <Title title="Series" list={Series} setModal={setModal} type="series" />
-      <Title title={DOC} list={Docs} setModal={setModal} type="documentary" />
+      {(Movies.length == 0) & (Series.length == 0) & (Docs.length == 0) ? (
+        <NoResults />
+      ) : (
+        <>
+          <Title
+            title="Movies"
+            list={Movies}
+            setModal={setModal}
+            type="movie"
+          />
+          <Title
+            title="Series"
+            list={Series}
+            setModal={setModal}
+            type="series"
+          />
+          <Title
+            title={DOC}
+            list={Docs}
+            setModal={setModal}
+            type="documentary"
+          />
+        </>
+      )}
       <Modal state={[modal, setModal]} />
     </section>
   );
