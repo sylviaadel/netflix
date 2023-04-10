b159d6c82bfb3bf65c4cc648531e90c47fdafe5c
Sylvia Tadross <sylvia.adel.emil@gmail.com>
2023-04-10 15:27:58 +0200

    refactor

--- src/components/landing/TopTenItem.jsx
+++ src/components/landing/TopTenItem.jsx
@@ -3,7 +3,7 @@ import DetailsPopup from "../modal/DetailsPopup";
 import Modal from "../modal/Modal";
 import { useItems } from "../../state/ItemsProvider";
 
-export default function TopTenItem({ item }) {
+export default function TopTenItem({ item, collection }) {
   const { topTenImg, heading } = item;
   const { uid } = useItems();
   const [id, setID] = useState("");
@@ -11,7 +11,9 @@ export default function TopTenItem({ item }) {
 
   function openDetails() {
     setID(uid);
-    setModal(<DetailsPopup item={item} />);
+    setModal(
+      <DetailsPopup item={item} collection={collection} setModal={setModal} />
+    );
   }
 
   return (
