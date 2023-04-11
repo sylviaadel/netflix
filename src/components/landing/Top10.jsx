1307c0fddaf814cec310a976dc9dd659778f3d2a
Sylvia Tadross <sylvia.adel.emil@gmail.com>
2023-04-11 23:27:44 +0200

    Add arrows to topten container

--- src/components/landing/Top10.jsx
+++ src/components/landing/Top10.jsx
@@ -2,11 +2,17 @@ import { useState, useEffect } from "react";
 import { readDocuments } from "../../scripts/fireStore/readDocuments";
 import { useItems } from "../../state/ItemsProvider";
 import TopTenItem from "./TopTenItem";
+import { useRef } from "react";
 
 export default function Top10({ collection }) {
   const { data, dispatch } = useItems();
   const [status, setStatus] = useState(0);
   const topTen = data.filter((item) => item.topTen === true);
+  const ref = useRef(null);
+
+  const scroll = (scrollOffset) => {
+    ref.current.scrollLeft += scrollOffset;
+  };
 
   useEffect(() => {
     loadData(collection);
@@ -32,7 +38,11 @@ export default function Top10({ collection }) {
   return (
     <section className="top10-container">
       <h3>Top 10 TV in Sweden Today</h3>
-      <ol>{Items}</ol>
+      <ol ref={ref}>
+        <i onClick={() => scroll(-100)} className="fa fa-chevron-left" />
+        {Items}
+        <i onClick={() => scroll(100)} className="fa fa-chevron-right" />
+      </ol>
     </section>
   );
 }
