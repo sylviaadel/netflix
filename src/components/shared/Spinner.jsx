fe28142a6942d749d2362a4a1929f30e403c7165
Sylvia Tadross <sylvia.adel.emil@gmail.com>
2023-04-14 12:35:08 +0200

    Change loader icon

--- src/components/shared/Spinner.jsx
+++ src/components/shared/Spinner.jsx
@@ -1,10 +1,7 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { solid } from "@fortawesome/fontawesome-svg-core/import.macro";
-
 export default function Spinner() {
   return (
     <div id="Spinner">
-      <FontAwesomeIcon icon={solid("circle-notch")} />
+      <div className="nfLoader"></div>
     </div>
   );
 }
