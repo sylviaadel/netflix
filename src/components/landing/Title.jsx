0a2d16a0cf946641b2c7722cb829f3b3e9de3639
Sylvia Tadross <sylvia.adel.emil@gmail.com>
2023-04-14 11:10:43 +0200

    Add arrows to titles list

--- src/components/landing/Title.jsx
+++ src/components/landing/Title.jsx
@@ -1,7 +1,14 @@
 import AddItem from "../modal/AddItem";
+import { useRef } from "react";
 
 export default function Title({ list, title, setModal, type }) {
   const collection = "titles";
+  const ref = useRef(null);
+
+  const scroll = (scrollOffset) => {
+    ref.current.scrollLeft += scrollOffset;
+  };
+
   function openForm() {
     setModal(
       <AddItem
@@ -20,7 +27,11 @@ export default function Title({ list, title, setModal, type }) {
           <h3>{title}</h3>
           <button onClick={openForm}>+ Add New</button>
           <div className="clear"></div>
-          <div className="titles-list">{list}</div>
+          <div className="titles-list" ref={ref}>
+            <i onClick={() => scroll(-100)} className="fa fa-chevron-left" />
+            {list}
+            <i onClick={() => scroll(100)} className="fa fa-chevron-right" />
+          </div>
         </div>
       )}
     </>
